465c4b60df040185ea7da6248b7225f252d5df12
hariharanramu <hariharan.ramu@gmail.com>
2021-02-11 18:23:56 +0530

    C1- Adding the Base Project

---
+++ SampleExercise/src/com/auto/First.java
@@ -0,0 +1,8 @@
+package com.auto;
+
+public class First {
+
+	public void display() {
+		System.out.println("Display !!!!!!!!!");
+	}
+}
---
+++ SampleExercise/src/com/auto/FirstChild.java
@@ -0,0 +1,10 @@
+package com.auto;
+
+public class FirstChild extends First{
+	
+	public static void main(String [] args) {
+		First f = new FirstChild();
+		f.display();
+	}
+
+}
